226eed722061b248e08e56fe8c121d67f8edcf8f
Dipesh Kumar Dutta <dipeshkumar.dutta@gmail.com>
2014-05-12 01:03:45 +0530

    Day1

---
+++ 1CommandLineParsing.py
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Name:        CommandLineParsing.py
+# Purpose:
+#
+# Author:      dkdutta
+#
+# Created:     06/05/2014
+# Copyright:   (c) dkdutta 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import sys;
+import getopt;
+
+def main():
+    sys.stdout.write("Number of arguments = " + str(len(sys.argv)) + "\n")
+    sys.stdout.write("Argument list = " + str(sys.argv)+"\n")
+
+    input_file='';
+    ouput_file='';
+    try:
+        opts, args = getopt.getopt(sys.argv, "hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        sys.stdout.write("CommandLineParsing.py -i <inputfile> -o <outputfile>'\n")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            sys.stdout.write("CommandLineParsing.py -i <inputfile> -o <outputfile>'\n")
+            sys.exit();
+        if opt in ("-i","-ifile"):
+            input_file = arg
+        if opt in ("-o","-ofile"):
+            ouput_file = arg
+    sys.stdout.write("Input File = " + input_file)
+    sys.stdout.write("Output File = " + ouput_file)
+
+if __name__ == '__main__':
+    main()
